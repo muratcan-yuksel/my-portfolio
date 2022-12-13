c5b32174e994ff325c935aa8355bb40e9b0df70f
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-12-13 17:02:27 +0300

    got about me data from strapi localhost

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
-import Navbar from "../components/Navbar";
+import axios from "axios";
 
-const index = () => {
+const endpoint = "http://localhost:1337/api/backgrounds";
+
+const index = ({ backgrounds }) => {
+  console.log(backgrounds);
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="h-screen border w-full md:w-4/6 lg:w-3/6 flex  justify-center ">
+      <div className="flex flex-col">
+        <h1 className="underline">About me</h1>
+
+        <h2>{backgrounds.data[0].attributes.about}</h2>
+      </div>
     </div>
   );
 };
 
 export default index;
+
+export async function getServerSideProps() {
+  const response = await axios.get(endpoint);
+  return {
+    props: {
+      backgrounds: response.data,
+    },
+  };
+}
