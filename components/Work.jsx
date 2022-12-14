b5278fdd013993c36c017a21e9728cbde8639521
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-12-14 16:58:26 +0300

    add first links

--- components/Work.jsx
+++ components/Work.jsx
@@ -7,21 +7,23 @@ import Link from "next/link";
 
 const Work = ({ title, description, image, demo, github, article }) => {
   return (
-    <div>
+    <div style={{ width: "40%" }} className=" flex flex-col items-center mx-5">
       <Image alt="" src={image}></Image>
       <h2>{title}</h2>
       <p>{description}</p>
-      <Link href={demo}>
-        <button className="heroButton">Demo</button>
-      </Link>{" "}
-      <Link href={github}>
-        <button className="heroButton">Source Code</button>
-      </Link>{" "}
-      {article ? (
-        <Link href={article}>
-          <button className="heroButton">Relevant Article</button>
-        </Link>
-      ) : null}
+      <div className="flex my-2">
+        <Link href={demo}>
+          <button className="heroButton">Demo</button>
+        </Link>{" "}
+        <Link href={github}>
+          <button className="heroButton">Source Code</button>
+        </Link>{" "}
+        {article ? (
+          <Link href={article}>
+            <button className="heroButton">Relevant Article</button>
+          </Link>
+        ) : null}
+      </div>
     </div>
   );
 };
--- pages/works.js
+++ pages/works.js
@@ -6,6 +6,25 @@ import wagmi from "../assets/wagmiSS.png";
 
 const projects = [
   {
+    title: "Wagmi Wallet Connect",
+    description:
+      "The user can connect to the dApp with different wallets and make transaction. The article I wrote explaining the build process was found by Alchemy, they ended up sending me some cool gifts for mentioning them in it",
+    image: wagmi,
+    demo: "https://muratcan-yuksel.github.io/wagmi-front3/",
+    github: "https://github.com/muratcan-yuksel/wagmi-post",
+    article:
+      "https://dev.to/muratcanyuksel/connecting-to-different-web3-wallets-using-wagmish-and-reactjs-1ojp",
+  },
+  {
+    title: "Wagmi",
+    description:
+      "Wagmi is a social media app for dog lovers. It's a full-stack app with a React Native front-end and a NodeJS back-end. I've used MongoDB for the database and deployed the app on Heroku. I've also used Firebase for the authentication and storage.",
+    image: wagmi,
+    demo: "https://wagmi.netlify.app/",
+    github: "",
+    article: "asd",
+  },
+  {
     title: "Wagmi",
     description:
       "Wagmi is a social media app for dog lovers. It's a full-stack app with a React Native front-end and a NodeJS back-end. I've used MongoDB for the database and deployed the app on Heroku. I've also used Firebase for the authentication and storage.",
@@ -19,20 +38,24 @@ const projects = [
 const works = () => {
   return (
     <div className="  h-screen p-2 border rounded-xl border-[#313131] px-5 w-full md:w-4/6 lg:w-3/6 flex flex-col ">
-      <h2 className="underline underline-offset-4 tracking-widest ">Works</h2>{" "}
-      {projects.map((item, index) => {
-        return (
-          <Work
-            key={index}
-            title={item.title}
-            description={item.description}
-            image={item.image}
-            demo={item.demo}
-            github={item.github}
-            article={item.article}
-          />
-        );
-      })}
+      <h2 className="underline underline-offset-4 tracking-widest mb-5">
+        Works
+      </h2>{" "}
+      <div className="flex flex-wrap justify-center">
+        {projects.map((item, index) => {
+          return (
+            <Work
+              key={index}
+              title={item.title}
+              description={item.description}
+              image={item.image}
+              demo={item.demo}
+              github={item.github}
+              article={item.article}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
