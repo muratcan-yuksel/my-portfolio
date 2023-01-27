af7ae147a631557886a9b3bbf2533e391f591917
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-01-27 04:17:35 +0300

    change axios version, add axios cache adapter

--- pages/videos.js
+++ pages/videos.js
@@ -1,19 +1,33 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Image from "next/image";
-
+import { setupCache } from "axios-cache-adapter";
 const Videos = () => {
   const [videos, setVideos] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Create `axios-cache-adapter` instance
+  const cache = setupCache({
+    maxAge: 15 * 60 * 1000,
+  });
+
+  // Create `axios` instance passing the newly created `cache.adapter`
+  const api = axios.create({
+    adapter: cache.adapter,
+  });
 
   async function getVideos() {
     try {
-      const res = await axios.get(
+      setIsLoading(true);
+      const res = await api.get(
         `https://www.googleapis.com/youtube/v3/search?key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}&channelId=${process.env.NEXT_PUBLIC_YOUTUBE_CHANNEL_ID}&part=snippet,id&order=date&maxResults=20`
       );
       setVideos(res.data.items);
       console.log(res.data.items);
     } catch (error) {
       console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   }
 
@@ -26,6 +40,7 @@ const Videos = () => {
       <h2 className="underline underline-offset-4 tracking-widest mb-5">
         Video Tutorials
       </h2>{" "}
+      {isLoading && <p>Loading...</p>}
       {videos &&
         videos.slice(0, -1).map((video, index) => (
           <div className="flex flex-wrap justify-center " key={index}>
